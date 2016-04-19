6f06e5b4264c3f319bd51ed30b3466281b59fda3
Matthias <git@myrho.net>
2016-04-19 10:35:34 +0200

    add median calc

--- src/Statistics.elm
+++ src/Statistics.elm
@@ -1,4 +1,4 @@
-module Statistics (divWithDefault, (/.)) where
+module Statistics (divWithDefault, (/.), median) where
 
 divWithDefault : Float -> Float -> Float -> Float
 divWithDefault default dividend divisor =
@@ -10,3 +10,15 @@ divWithDefault default dividend divisor =
 
 (/.) = 
   divWithDefault 0
+
+median : List Float -> Float
+median values =
+  let
+    len =
+      List.length values
+  in
+    List.sort values
+      |> List.drop ((toFloat len ) / 2 |> ceiling |> (\c -> c - 1))
+      |> List.take (2 - (len % 2))
+      |> List.sum
+      |> (\s -> s / (toFloat <| 2 - len % 2))
