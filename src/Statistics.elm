692ca03b66aa87812491246b2d2431ddb116f0c6
Matthias <git@myrho.net>
2016-12-13 11:58:42 +0100

    add quantileR7

--- src/Statistics.elm
+++ src/Statistics.elm
@@ -1,24 +1,68 @@
-module Statistics exposing (divWithDefault, (/.), median) 
+module Statistics exposing (divWithDefault, (/.), median, quantileR7)
+
+{-|
+A growing collection of functions useful for statistics.
+-}
+
+import Array
+
 
 divWithDefault : Float -> Float -> Float -> Float
 divWithDefault default dividend divisor =
-  if divisor == 0
-    then 
-      default
+    if divisor == 0 then
+        default
     else
-      dividend / divisor
+        dividend / divisor
 
-(/.) = 
-  divWithDefault 0
 
+(/.) =
+    divWithDefault 0
+
+
+{-|
+Calculate the median of a list of values
+
+    median [1,1,1,3,4,7,9,11,13,13] = 5.5
+-}
 median : List Float -> Float
-median values =
-  let
-    len =
-      List.length values
-  in
-    List.sort values
-      |> List.drop ((toFloat len ) / 2 |> ceiling |> (\c -> c - 1))
-      |> List.take (2 - (len % 2))
-      |> List.sum
-      |> (\s -> s / (toFloat <| 2 - len % 2))
+median =
+    quantileR7 0.5
+
+
+{-|
+Calculate the quantile of a list of values using formula R-7
+(https://en.wikipedia.org/wiki/Quantile).
+
+    quantile 0.3 [1,1,1,3,4,7,9,11,13,13] = 2.4
+    quantile 0.5 [1,1,1,3,4,7,9,11,13,13] = 5.5 -- the median
+-}
+quantileR7 : Float -> List Float -> Float
+quantileR7 p values =
+    let
+        p_clamped =
+            clamp 0 1 p
+
+        v =
+            Array.fromList <|
+                List.sort values
+
+        n =
+            List.length values
+
+        h =
+            ((toFloat n) - 1) * p
+
+        h_floor =
+            floor h
+
+        x_h_floor =
+            Array.get h_floor v
+                |> Maybe.withDefault 0
+
+        x_h_plus1 =
+            Array.get (h_floor + 1) v
+                |> Maybe.withDefault 0
+    in
+        x_h_floor
+            + (h - (toFloat h_floor))
+            * (x_h_plus1 - x_h_floor)
